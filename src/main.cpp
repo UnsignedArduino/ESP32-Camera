6b9733f2807e286d7ae3af1e155294ecf64e2188
UnsignedArduino <UnsignedArduino@outlook.com>
2023-01-11 17:13:18 -0500

    RTC works

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,47 @@
 #include <Arduino.h>
+#include <RTClib.h>
 #include <SPI.h>
 
 #include "TFT_eSPI.h"
 
+#define halt()   \
+  while (true) { \
+    ;            \
+  }
+
 TFT_eSPI tft = TFT_eSPI();
+RTC_DS3231 rtc;
 
 void setup(void) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
-  
+
   tft.init();
   tft.fillScreen(TFT_BLACK);
   tft.setRotation(1);
 
-  tft.print("Hello, world!");
+  tft.println();
+  tft.println();
+
+  if (!rtc.begin()) {
+    tft.println("Couldn't find RTC");
+    halt();
+  }
+
+  if (rtc.lostPower()) {
+    tft.println("RTC lost power, setting to compile time");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop() {
-  ;
+  DateTime now = rtc.now();
+
+  tft.fillRect(0, 0, tft.width(), 8, TFT_BLACK);
+  tft.setCursor(0, 0);
+  tft.printf("%d/%d/%d %.2d:%.2d:%.2d", now.year(), now.month(), now.day(),
+             now.hour(), now.minute(), now.second());
+
+  delay(1000);
 }
