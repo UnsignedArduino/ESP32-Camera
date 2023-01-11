b77a215fe46384b1a1452ae1b4eeb049d85f8ffb
UnsignedArduino <UnsignedArduino@outlook.com>
2023-01-11 18:34:51 -0500

    SD card works

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,41 @@
 #include <RTClib.h>
 #include <SPI.h>
 
+#include "SdFat.h"
 #include "TFT_eSPI.h"
 
-#define halt()   \
-  while (true) { \
-    ;            \
-  }
+const uint8_t SD_CS = 16;
 
 TFT_eSPI tft = TFT_eSPI();
 RTC_DS3231 rtc;
+SdFs sd;
+
+void printDirectory(FsFile dir, uint16_t numTabs) {
+  while (true) {
+    FsFile entry = dir.openNextFile();
+    if (!entry) {
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      tft.print("  ");
+    }
+    char name[255] = {};
+    entry.getName(name, 255);
+    tft.print(name);
+    if (entry.isDirectory()) {
+      tft.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      tft.printf(" (%lu)\n", entry.size());
+    }
+    entry.close();
+  }
+}
 
 void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
 
-  tft.init();
+  tft.begin();
   tft.fillScreen(TFT_BLACK);
   tft.setRotation(1);
 
@@ -26,13 +45,27 @@ void setup(void) {
 
   if (!rtc.begin()) {
     tft.println("Couldn't find RTC");
-    halt();
   }
 
   if (rtc.lostPower()) {
     tft.println("RTC lost power, setting to compile time");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+
+  if (!sd.begin(SD_CS, SD_SCK_MHZ(16))) {
+    tft.println("Couldn't find SD card");
+    if (sd.sdErrorCode()) {
+      tft.print("SD error code: ");
+      printSdErrorSymbol(&tft, sd.sdErrorCode());
+      tft.print("\nSD error data: ");
+      tft.println(sd.sdErrorData());
+    }
+  }
+
+  tft.println("SD card contents: ");
+  FsFile root = sd.open("/");
+  printDirectory(root, 0);
+  root.close();
 }
 
 void loop() {
