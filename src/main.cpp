6d29adff44b3008c9aec98a802800d36c88eae1f
UnsignedArduino <UnsignedArduino@outlook.com>
2023-01-11 23:34:20 -0500

    Appears to transmit correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
 #include <RTClib.h>
 #include <SPI.h>
-
-#include "SdFat.h"
-#include "TFT_eSPI.h"
+#include <SdFat.h>
+#include <TFT_eSPI.h>
+#include <ArduCAM.h>
+#include <memorysaver.h>
 
 const uint8_t SD_CS = 16;
+const uint8_t CAM_CS = 5;
+const uint8_t CAM_MISO_TRISTATE = 17;
 
-TFT_eSPI tft = TFT_eSPI();
 RTC_DS3231 rtc;
+TFT_eSPI tft = TFT_eSPI();
 SdFs sd;
+ArduCAM camera(OV2640, CAM_CS);
 
 void printDirectory(FsFile dir, uint16_t numTabs) {
   while (true) {
@@ -31,6 +35,105 @@ void printDirectory(FsFile dir, uint16_t numTabs) {
     }
     entry.close();
   }
+}
+
+bool cameraBegin() {
+  Serial.println("Preparing camera...");
+
+  Wire.begin();
+
+  SPI.begin();
+  SPI.setFrequency(8000000);
+
+  pinMode(CAM_CS, OUTPUT);
+  pinMode(CAM_MISO_TRISTATE, OUTPUT);
+  digitalWrite(CAM_MISO_TRISTATE, HIGH);
+
+  Serial.print("Testing SPI...");
+
+  camera.write_reg(ARDUCHIP_TEST1, 0x55);
+  uint8_t temp = camera.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55) {
+    Serial.print("error! (0x55 != 0x");
+    Serial.print(temp, HEX);
+    Serial.println(")");
+    return false;
+  } else {
+    Serial.println("ok!");
+  }
+
+  Serial.print("Checking camera presence...");
+
+  uint8_t vid, pid;
+  camera.wrSensorReg8_8(0xff, 0x01);
+  camera.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  camera.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if ((vid != 0x26) && ((pid != 0x41) || (pid != 0x42))) {
+    Serial.println("missing! (vid: 0x");
+    Serial.print(vid, HEX);
+    Serial.print(" pid: 0x");
+    Serial.print(pid, HEX);
+    Serial.println(")");
+    return false;
+  } else {
+    Serial.println("found!");
+  }
+
+  camera.set_format(JPEG);
+  camera.InitCAM();
+  camera.OV2640_set_JPEG_size(OV2640_160x120);
+  camera.clear_fifo_flag();
+
+  digitalWrite(CAM_MISO_TRISTATE, LOW);
+
+  Serial.println("Camera ok!");
+
+  return true;
+}
+
+void cameraCapture() {
+  Serial.println("Starting capture");
+
+  digitalWrite(CAM_MISO_TRISTATE, HIGH);
+
+  camera.clear_fifo_flag();
+  camera.start_capture();
+
+  while (!camera.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK)) {
+    ;
+  }
+
+  size_t len = camera.read_fifo_length();
+  if (len >= 0x07ffff) {
+    Serial.println("Image over size");
+    return;
+  } else if (len == 0) {
+    Serial.println("Image size is 0");
+    return;
+  } else {
+    Serial.print("Image size is ");
+    Serial.println(len);
+  }
+
+  camera.CS_LOW();
+  camera.set_fifo_burst();
+
+  SPI.transfer(0xFF);
+
+  static const size_t bufferSize = 4096;
+  static uint8_t buffer[bufferSize] = {0xFF};
+
+  while (len > 0) {
+    size_t will_copy = (len < bufferSize) ? len : bufferSize;
+    SPI.transferBytes(&buffer[0], &buffer[0], will_copy);
+    // client.write(&buffer[0], will_copy);
+    len -= will_copy;
+  }
+
+  camera.CS_HIGH();
+  digitalWrite(CAM_MISO_TRISTATE, LOW);
+
+  Serial.println("Camera capture finished");
 }
 
 void setup(void) {
@@ -66,6 +169,14 @@ void setup(void) {
   FsFile root = sd.open("/");
   printDirectory(root, 0);
   root.close();
+
+  if (!cameraBegin()) {
+    tft.println("Couldn't find camera");
+  }
+
+  tft.println("Starting capture");
+  cameraCapture();
+  tft.println("Capture finished");
 }
 
 void loop() {
